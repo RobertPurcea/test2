c1ab842375d687daa50d540fd8b1b95856a08e16
RobertPurcea <purcearobert@gmail.com>
2019-07-02 12:47:27 +0100

    Updates

---
+++ precache-manifest.716cb91149fe3bee1635c1ae9b36a0b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c455a88bd1c4779c0417670964c19c4",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "c59462d8ef9b9264aa96",
+    "url": "/test2/static/css/main.d231bbd4.chunk.css"
+  },
+  {
+    "revision": "255c4cb2ee8a677e6f09",
+    "url": "/test2/static/js/2.23b9ed4d.chunk.js"
+  },
+  {
+    "revision": "c59462d8ef9b9264aa96",
+    "url": "/test2/static/js/main.77f74a3a.chunk.js"
+  },
+  {
+    "revision": "872033a9efb6eb9fe611",
+    "url": "/test2/static/js/runtime~main.bb481ce1.js"
+  }
+]);
